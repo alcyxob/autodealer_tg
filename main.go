c605043dd5f87d14e4b2b3b0723dd1819d33c385
Oleksandr Vorobiov <ovor@softserveinc.com>
2019-08-06 15:26:45 +0300

    Added tg code

--- main.go
+++ main.go
@@ -1,31 +1,82 @@
 package main
 
 import (
+	"bufio"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
 )
 
-// Handler is executed by AWS Lambda in the main function. Once the request
-// is processed, it returns an Amazon API Gateway response object to AWS Lambda
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	index, err := ioutil.ReadFile("public/index.html")
+func main() {
+	bot, err := tgbotapi.NewBotAPI("")
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		log.Panic(err)
 	}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(index),
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-	}, nil
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// инициализируем канал, куда будут прилетать обновления от API
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	upd, _ := bot.GetUpdatesChan(ucfg)
+	// читаем обновления из канала
+	for {
+		select {
+		case update := <-upd:
+			UserName := update.Message.From.UserName
+
+			ChatID := update.Message.Chat.ID
+
+			Text := update.Message.Text + " tvar"
+
+			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+
+			// Ответим пользователю его же сообщением
+			//reply := Text
+			reply := getAudi()
+			// Созадаем сообщение
+			msg := tgbotapi.NewMessage(ChatID, reply)
+			// и отправляем его
+			bot.Send(msg)
+		}
+
+	}
 }
 
-func main() {
-	lambda.Start(Handler)
+func getAudi() string {
+	url := "https://auto.ria.com/search/?categories.main.id=1&brand.id[0]=6&model.id[0]=49&year[0].gte=2009&year[0].lte=2012&region.id[0]=7&region.id[1]=10&gearbox.id[0]=2&gearbox.id[1]=3&gearbox.id[2]=4&gearbox.id[3]=5&drive.id[0]=1&abroad.not=0&custom.not=1&page=0&size=100"
+	resp, err := http.Get(url)
+	response := "not found"
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(body)))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "window.ria.server.ids") {
+			response = line
+		}
+	}
+
+	riaIdsDirty := strings.Split(response, "=")[1]
+	riaIdsDirty = strings.Replace(riaIdsDirty, "[", "", -1)
+	riaIdsDirty = strings.Replace(riaIdsDirty, "]", "", -1)
+	riaIdsDirty = strings.Replace(riaIdsDirty, ";", "", -1)
+	riaIdsClean := strings.Replace(riaIdsDirty, "\"", "", -1)
+	//riaIds := strings.Split(riaIdsClean, ",")
+
+	return riaIdsClean
 }
